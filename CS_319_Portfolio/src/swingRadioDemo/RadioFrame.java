e04f3a8f72ef750f94a70992951f62f2bf3a025f
Matt Burzinski <matthewburzinski@gmail.com>
2014-09-14 16:38:17 -0500

    test Commit

---
+++ CS_319_Portfolio/src/swingRadioDemo/RadioFrame.java
@@ -0,0 +1,26 @@
+package swingRadioDemo;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class RadioFrame extends JFrame implements ActionListener{
+
+	JPanel radioPanel;
+	JButton playButton;
+	JButton stopButton;
+	
+	public RadioFrame(){
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
